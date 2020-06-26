d8816bd52bd7ebf27ff77a354dcc6de6e7f71a52
datptse05288 <datptse05288@fpt.edu.vn>
2020-06-26 10:40:48 +0700

    Initial commit

---
+++ src/main/java/com/timebird/scheduleGetData/ScheduleGetDataApplication.java
@@ -0,0 +1,13 @@
+package com.timebird.scheduleGetData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleGetDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleGetDataApplication.class, args);
+	}
+
+}
